748987a87a05f07fae1dc7cafa5212206d32204b
satish559k <satishnadar559@gmail.com>
2025-11-08 15:39:51 +0530

    Initial commit

---
+++ src/main/java/com/InventoryModule/InventoryModule/InventoryModuleApplication.java
@@ -0,0 +1,13 @@
+package com.InventoryModule.InventoryModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InventoryModule/InventoryModule/InventoryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InventoryModule.InventoryModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
